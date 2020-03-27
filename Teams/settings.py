9e2cd370657f53cd553df12f99ffebcdd724fb79
Ho <cho@kor1.onmicrosoft.com>
2020-03-27 18:10:05 +0900

    css mod2

--- Teams/settings.py
+++ Teams/settings.py
@@ -124,4 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/site/wwwroot/chart/static/'
+STATIC_ROOT = '/home/site/wwwroot/chart/static_root/'
+STATIC_DIRS = ['/home/site/wwwroot/chart/static/']
