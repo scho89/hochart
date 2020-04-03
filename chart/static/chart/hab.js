79d13407ee38c62dd0873234ce7e3cafcb31fa66
Ho <cho@kor1.onmicrosoft.com>
2020-04-03 17:33:25 +0900

    update HAB

---
+++ chart/static/chart/hab.js
@@ -0,0 +1,11 @@
+function expandgroup(groupclass){
+     var objDiv = document.getElementsByClassName(groupclass);
+     if(objDiv.style.display=="block"){ objDiv.style.display = "none"; }
+      else{ objDiv.style.display = "block"; }
+}
+
+function expand(id){
+      var objGroup = document.getElementById(id);
+      if(objGroup.style.display!="none"){objGroup.style.display = "none";}
+      else{objGroup.style.display = "block"}      
+}
