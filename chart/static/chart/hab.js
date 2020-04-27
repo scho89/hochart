deb486ee339f24c51cdeab4d40c9c625e4b66a1e
Ho <cho@kor1.onmicrosoft.com>
2020-04-27 14:57:40 +0900

    Add mailto link

--- chart/static/chart/hab.js
+++ chart/static/chart/hab.js
@@ -10,10 +10,3 @@ function detail(id){
       else{objGroup.style.display = "inline-block";}      
 }
 
-function handler( event ) {
-      var target = $( event.target );
-      if ( target.is( "li" ) ) {
-        target.children().toggle();
-      }
-    }
-    $( "ul" ).click( handler ).find( "ul" ).hide();
